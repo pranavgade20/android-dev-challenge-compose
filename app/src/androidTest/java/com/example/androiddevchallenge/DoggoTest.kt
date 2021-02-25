cd296c66b735c68c087bcae13fcdc0f228d5c974
Pranav Gade <pranavgade20@gmail.com>
2021-02-26 00:41:48 +0530

    added tests

---
+++ app/src/androidTest/java/com/example/androiddevchallenge/DoggoTest.kt
@@ -0,0 +1,12 @@
+package com.example.androiddevchallenge
+
+import junit.framework.TestCase
+
+class DoggoTest : TestCase() {
+
+    fun testTestGetName() {}
+
+    fun testGetAge() {}
+
+    fun testGetBreed() {}
+}
